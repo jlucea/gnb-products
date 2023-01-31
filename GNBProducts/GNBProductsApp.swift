b95b921d76590e0702607f29794b2f0f654fa4d9
Jaime Lucea <jaime.lucea@atactic.io>
2023-01-31 14:51:56 +0100

    Initial Commit

---
+++ GNBProducts/GNBProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNBProductsApp.swift
+//  GNBProducts
+//
+//  Created by Jaime Lucea on 31/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GNBProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
